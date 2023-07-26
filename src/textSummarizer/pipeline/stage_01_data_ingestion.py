8cdeba6d40ffd15e69f23d15ac626c9a5498271a
anujvaghani0 <anujvaghani0@gmail.com>
2023-07-26 11:56:23 +0530

    data ingestion pipeline added

---
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,27 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.conponents.data_ingestion import DataIngestion
+from textSummarizer.logging import logger
+
+# ssl setup for mac
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+    
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
